abfb1d3a276538bdf63981f78957de5e8513636b
Abdón Senen Meléndez Díaz <alu0101539669@ull.edu.es>
2025-04-23 10:15:08 +0000

    evaluacion

---
+++ dist/modi/findSpell.d.ts
@@ -0,0 +1,2 @@
+import request from "request";
+export declare function findSpell(name?: string, type?: string, incantation?: string): Promise<request.Response>;
--- dist/server.js
+++ dist/server.js
@@ -3,24 +3,28 @@ import { addFunko, deleteFunko, getFunko, listFunkos, updateFunko } from './mana
 const app = express();
 app.use(express.json());
 const port = 3000;
+//Añadir un nuevo Funko
 app.post('/funkos', async (req, res) => {
     const user = req.query.user;
     const funko = req.body;
     const response = await addFunko(user, funko);
     res.json(response);
 });
+//Actualizar un Funko existente
 app.patch('/funkos', async (req, res) => {
     const user = req.query.user;
     const funko = req.body;
     const response = await updateFunko(user, funko);
     res.json(response);
 });
+//Eliminar un Funko existente
 app.delete('/funkos', async (req, res) => {
     const user = req.query.user;
     const id = Number(req.query.id);
     const response = await deleteFunko(user, id);
     res.json(response);
 });
+//Listar todos los Funkos de un usuario
 app.get('/funkos', async (req, res) => {
     const user = req.query.user;
     if (req.query.id) {
@@ -36,3 +40,4 @@ app.get('/funkos', async (req, res) => {
 app.listen(port, () => {
     console.log(`Servidor escuchando en el puerto ${port}`);
 });
+export default app;
